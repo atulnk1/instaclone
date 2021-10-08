965ecb5f9f4f91079f8360e45a1a9ac40c2d9013
atulnk <atulnk1@gmail.com>
2021-10-08 17:54:11 +0800

    added my posts route

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,6 +4,46 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const Post = require("../models/post");
 
+// SHOW ALL POSTS
+
+controller.get('/allposts', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+        const posts = await Post.find({})
+        // .populate("postedBy", "_id name")
+        .populate([
+            {
+                path: 'postedBy',
+                select: ['_id','name']
+            }])
+        res.json({posts})
+    } catch(e) {
+        return res.status(400).json({
+            name: e.name,
+            message: e.message
+        })
+    }
+})
+
+// GET ALL POSTS FOR A PARTICULAR USER
+
+controller.get('/myposts', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+        console.log(req.user._id)
+        const myPosts = await Post.find({postedBy:req.user._id})
+        .populate([
+            {
+                path: 'postedBy',
+                select: ['_id','name']
+            }])
+        return res.json({myPosts})
+    } catch(e) {
+        return res.status(400).json({
+            name: e.name,
+            message: e.message
+        })
+    }
+})
+
 // CREATE NEW POST
 controller.post('/newpost', passport.authenticate('jwt', { session: false }), async (req, res) => {
     try {
@@ -41,5 +81,7 @@ controller.post('/newpost', passport.authenticate('jwt', { session: false }), as
     }
     
 })
+
+
 
 module.exports = controller

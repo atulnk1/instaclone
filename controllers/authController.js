04bc8de7c8134e49e4df9c8c603635c512fa8e05
atulnk <atulnk1@gmail.com>
2021-10-08 14:52:49 +0800

    basic sign in and sign up done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,97 @@
 const express = require('express');
 const controller = express.Router();
+const mongoose = require('mongoose');
+const User = require("../models/user");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config/keys');
 
-controller.post('/signup', (req, res) => {
-    const { name, email, password } = req.body;
-    console.log(req.body)
-    if(!name || !email || !password) {
-        // 422 error: Server has understood the request but can not process it
-        return res.status.apply(422).json({error: "Fields are missing"})
-    } 
-    res.json({message: "Successful login"})
+
+// Test to see if the passport middleware works
+// const passport = require('passport')
+
+// controller.get('/protected', passport.authenticate('jwt', { session: false }),(req, res) => {
+//     return res.json("protected route accessed")
+// })
+
+
+controller.post('/signup', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+        // console.log(req.body)
+        if(!name || !email || !password) {
+            // 422 error: Server has understood the request but can not process it
+            return res.status(422).json({error: "Fields are missing"})
+        } 
+
+
+        const existingUser = await User.findOne({email: email});
+
+        if(existingUser){
+            return res.status(422).json({error: "User already exits. Please register with another email."})
+        } else {
+            try {
+                const inputs = {
+                    name: name,
+                    email: email,
+                    password: hashedPassword
+                }
+
+                await User.create(inputs)
+
+                return res.status(200).json({message: "User successfully created"})
+
+
+            } catch(e) {
+                return res.status(400).json({
+                    name: e.name,
+                    message: e.message
+                })
+            }
+        }
+
+    } catch (e) {
+        return res.status(400).json({
+            name: e.name,
+            message: e.message
+        })
+    }
+    
+})
+
+
+controller.post('/signin', async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        if(!email || !password) {
+            return res.status(422).json({error: "Fields are missing"})
+        } 
+
+        const existingUser = await User.findOne({email: email});
+
+        if(!existingUser) {
+            return res.status(401).json({error: "Invalid Email or Password. Please try again."})
+        } else {
+            const isCorrectPassword = bcrypt.compareSync(password, existingUser.password)
+
+            if(!isCorrectPassword) {
+                return res.status(401).json({error: "Invalid Email or Password. Please try again."})
+            } else {
+                // return res.json({message: "User logged in"})
+                const token = jwt.sign({_id: existingUser._id}, JWT_SECRET)
+                return res.json({token})
+            }
+        }
+
+    } catch (e) {
+        return res.status(400).json({
+            name: e.name,
+            message: e.message
+        })
+    }
+
 })
 
 
--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-const { MONGO_URI } = require('./config/keys');
 
+// Passport setup for token verification
+const passport = require('passport');
+const strategy = require('./passport');
+
+passport.use(strategy);
+
+const { MONGO_URI } = require('./config/keys');
+const app = express();
+
+// Required Controllers
 const authController = require('./controllers/authController');
 
-
+// DB Connection methods
 const dbConnection = mongoose.connection;
-
 mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
-
 dbConnection.on("connected", () => console.log("Database Connected Successfully"));
 dbConnection.on("error", (err) => console.log(`Got error! ${err.message}`));
 dbConnection.on("disconnected", () => console.log("My database is disconnected"));
 
 require('./models/user');
 
-
+// Additional middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
 
 
 // Linking to the controllers
-app.use(authController);
+app.use('/api', authController);
 
 app.listen(process.env.PORT)

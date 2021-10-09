524971529713c1b9001db6f424d028bd2b8c1760
atulnk <atulnk1@gmail.com>
2021-10-09 11:48:32 +0800

    added follow/unfollow route

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const Post = require("../models/post");
 const User = require("../models/user");
 
+// FIND USER INFO ROUTE, TO POPULATE PROFILE PAGE
 controller.get('/profile/:userId', passport.authenticate('jwt', { session: false }), async(req, res) =>{
     try {
         const findUser = await User.findById(
@@ -12,7 +13,7 @@ controller.get('/profile/:userId', passport.authenticate('jwt', { session: false
             '-password'
             )
         if(!findUser) {
-            res.status(404).json({error:"Sorry, user not found."})
+            res.status(422).json({error:"Sorry, user not found."})
         } else {
             try {
                 const findPosts = await Post.find({
@@ -25,8 +26,9 @@ controller.get('/profile/:userId', passport.authenticate('jwt', { session: false
                 }])
 
                 if(!findPosts){
-                    res.status(422).json({error: "Unable to retrieve posts by user. Please try again."})
+                    res.status(422).json({error: "Unable to retrieve posts for user. Please try again."})
                 } else {
+                    // returns details on queried user and all associated posts for said user
                     res.json({findUser, findPosts})
                 }
             } catch(e) {
@@ -44,7 +46,101 @@ controller.get('/profile/:userId', passport.authenticate('jwt', { session: false
     }
 })
 
+// FOLLOW ROUTE - UPDATES LIST OF FOLLOWERS AND FOLLOWING
+controller.put('/follow', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+        // Update the list of followers for a user that the logged in user is trying to follow
+        const updateFollowers = await User.findByIdAndUpdate(
+            {_id: req.body.followId},
+            {
+                $addToSet:{followers:req.user._id}
+            }, {
+                new: true
+            }
+        )
+
+        if(!updateFollowers) {
+            res.status(422).json({error: "Unable to follow this user. Try again later."})
+        } else {
+            try {
+                // Update the list of following for the current logged in user
+                const updateFollowing = await User.findByIdAndUpdate(
+                    req.user._id,
+                    // "-password",
+                    {
+                        $addToSet:{following:req.body.followId}
+                    }, {
+                        new: true,
+                        select: '-password'
+                    }
+                )
+                if(!updateFollowing){
+                    res.status(422).json({error: "Unable to follow this user. Try again later."})
+                } else {
+                    res.json({updateFollowing})
+                }
+            } catch(e) {
+                return res.status(400).json({
+                    name: e.name,
+                    message: e.message
+                })
+            }
+        }
+    } catch(e) {
+        return res.status(400).json({
+            name: e.name,
+            message: e.message
+        })
+    }
+})
 
 
+// UNFOLLOW ROUTE - UPDATES LIST OF FOLLOWERS AND FOLLOWING
+controller.put('/unfollow', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+        // Update the list of followers for a user that the logged in user is trying to follow
+        const updateFollowers = await User.findByIdAndUpdate(
+            {_id: req.body.followId},
+            {
+                $pull:{followers: req.user._id}
+            }, {
+                new: true
+            }
+        )
+
+        if(!updateFollowers) {
+            res.status(422).json({error: "Unable to unfollow this user. Try again later."})
+        } else {
+            try {
+                // Update hte list of following for the current logged in user
+                const updateFollowing = await User.findByIdAndUpdate(
+                    req.user._id,
+                    // "-password",
+                    {
+                        $pull:{following:req.body.followId}
+                    }, {
+                        new: true,
+                        select: '-password'
+                    }
+                )
+                if(!updateFollowing){
+                    res.status(422).json({error: "Unable to unfollow this user. Try again later."})
+                } else {
+                    res.json({updateFollowing})
+                }
+            } catch(e) {
+                return res.status(400).json({
+                    name: e.name,
+                    message: e.message
+                })
+            }
+        }
+    } catch(e) {
+        return res.status(400).json({
+            name: e.name,
+            message: e.message
+        })
+    }
+})
 
 module.exports = controller;
--- models/user.js
+++ models/user.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const { ObjectId } = mongoose.Schema.Types;
 
 const userSchema = new Schema (
     {
         name: { type: String, required: true},
         email: { type: String, required: true, unqiue: true},
-        password: { type: String, required: true }
+        password: { type: String, required: true },
+        followers: [{ type: ObjectId, ref:"User" }],
+        following: [{ type: ObjectId, ref:"User" }]
+
     },
     {
         timestamps: true

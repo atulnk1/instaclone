8198853cd11b455751d3726e1ba7b56f59a73e0f
darrylwongqz <dwqz92@gmail.com>
2021-10-18 00:31:59 +0800

    modified server side code, added skeletal structure of react app, installed client side dependencies, framed out login and sign up pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 
-function App() {
+//Enables lazy loading --> splits build into separate js bundles that will be delivered on demand vs one whole pack
+//Improves performance of app once it gets bigger
+const Login = lazy(() => import("./pages/Login"));
+const SignUp = lazy(() => import("./pages/Sign-up"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Profile = lazy(() => import("./pages/Profile"));
+const NotFound = lazy(() => import("./pages/Not-found"));
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.PROFILE} component={Profile} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
-
-export default App;
---
+++ client/src/constants/routes.js
@@ -0,0 +1,5 @@
+export const DASHBOARD = "/";
+export const LOGIN = "/Login";
+export const SIGN_UP = "/Sign-up";
+export const PROFILE = "/profile/:username";
+export const NOT_FOUND = "/Not-found";
